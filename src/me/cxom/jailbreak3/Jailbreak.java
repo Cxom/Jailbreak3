b1c8069fa32d5b93f285336629cff64621ce7723
Cxom <jeremybk@sbcglobal.net>
2017-02-09 17:49:25 -0500

    Add addPlayer, removePlayer methods, space to chat prefix in Jailbreak

--- src/me/cxom/jailbreak3/Jailbreak.java
+++ src/me/cxom/jailbreak3/Jailbreak.java
@@ -6,25 +6,20 @@ import java.util.UUID;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.cxom.jailbreak3.arena.JailbreakArena;
 import me.cxom.jailbreak3.arena.config.ArenaManager;
 import me.cxom.jailbreak3.events.CancelledEvents;
 import me.cxom.jailbreak3.events.CommandEvents;
 import me.cxom.jailbreak3.events.custom.JailbreakDeathEventCaller;
-import me.cxom.jailbreak3.game.GameInstance;
-import me.cxom.jailbreak3.player.JailbreakMenu;
 import me.cxom.jailbreak3.player.JailbreakPlayer;
 import me.cxom.jailbreak3.player.PlayerProfile;
 
 public class Jailbreak extends JavaPlugin{
 	
-	public static final String CHAT_PREFIX = ChatColor.BLUE + "[" + ChatColor.WHITE + "Jailbreak" + ChatColor.BLUE + "]" + ChatColor.RESET;
+	public static final String CHAT_PREFIX = ChatColor.BLUE + "[" + ChatColor.WHITE + "Jailbreak" + ChatColor.BLUE + "]" + ChatColor.RESET + " ";
 	
 	private static Plugin plugin;
 	public static Plugin getPlugin(){ return plugin; }
@@ -43,7 +38,8 @@ public class Jailbreak extends JavaPlugin{
 	
 	@Override
 	public void onDisable(){
-
+		PlayerProfile.restoreAll();
+		//deal with arena files (creation/modification saving)
 	}
 	
 	public static boolean isPlayer(Player player){ return isPlayer(player.getUniqueId()); }
@@ -54,6 +50,16 @@ public class Jailbreak extends JavaPlugin{
 	public static JailbreakPlayer getPlayer(Player player){ return getPlayer(player.getUniqueId()); }
 	public static JailbreakPlayer getPlayer(UUID uuid){
 		return players.get(uuid);
+	}
+	
+	public static void addPlayer(JailbreakPlayer jp){
+		players.put(jp.getUniqueId(), jp);
+	}
+	
+	public static void removePlayer(JailbreakPlayer jp){ removePlayer(jp.getUniqueId()); }
+	public static void removePlayer(Player player){ removePlayer(player.getUniqueId()); }
+	public static void removePlayer(UUID uuid){
+		players.remove(uuid);
 	}
 	
 }
