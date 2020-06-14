f68eb929ecd93501ec494a84b56562f17ca194ca
Cxom <cxomtdoh@yahoo.com>
2020-06-14 13:51:18 -0400

    Prevent block breaking and placing explicitly (by administrators)

--- src/me/cxom/jailbreak3/events/CancelledEvents.java
+++ src/me/cxom/jailbreak3/events/CancelledEvents.java
@@ -3,6 +3,8 @@ package me.cxom.jailbreak3.events;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
@@ -13,10 +15,30 @@ import me.cxom.jailbreak3.Jailbreak;
 public class CancelledEvents implements Listener{
 
 	@EventHandler
-	public void onFallDamage(EntityDamageEvent e) {
+	public void onProtectedDamage(EntityDamageEvent e) {
 		if (e.getEntity() instanceof Player
-				&& e.getCause() == DamageCause.FALL
+				&& isProtectedDamageCause(e)
 				&& Jailbreak.isPlayer((Player) e.getEntity())){
+			e.setCancelled(true);
+		}
+	}
+	
+	private boolean isProtectedDamageCause(EntityDamageEvent e) {
+		DamageCause cause = e.getCause();
+		return cause == DamageCause.FALL
+			|| cause == DamageCause.ENTITY_EXPLOSION;
+	}
+	
+	@EventHandler
+	public void onPlayerBlockBreak(BlockBreakEvent e) {
+		if (Jailbreak.isPlayer(e.getPlayer())) {
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerBlockPlace(BlockPlaceEvent e) {
+		if (Jailbreak.isPlayer(e.getPlayer())) {
 			e.setCancelled(true);
 		}
 	}
