1668f50e098a3ba0c21d5d9c9119406afc1d282d
Cxom <cxomtdoh@yahoo.com>
2020-08-01 09:34:16 -0400

    Fix iron bar jail doors not connecting to adjacent solid blocks

--- src/me/cxom/jailbreak3/arena/Door.java
+++ src/me/cxom/jailbreak3/arena/Door.java
@@ -2,12 +2,20 @@ package me.cxom.jailbreak3.arena;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.MultipleFacing;
 import org.bukkit.event.Listener;
 
 import net.punchtree.minigames.region.Area;
 
 public class Door implements Listener{
 
+	private static final BlockFace[] SIDE_FACES = { 
+			BlockFace.WEST,
+			BlockFace.NORTH,
+			BlockFace.EAST, 
+			BlockFace.SOUTH };
+	
 	private final Area door;
 	
 	private boolean open;
@@ -25,9 +33,23 @@ public class Door implements Listener{
 	
 	public void close(){
 		if (!open) return;
-		for(Block b : door)
+		for(Block b : door) {
 			b.setType(Material.IRON_BARS);
+			MultipleFacing multiFacing = (MultipleFacing) b.getBlockData();
+			for (BlockFace face : SIDE_FACES) {
+				Material relative = b.getRelative(face).getType();
+				if (relative.isSolid() && !isSlab(relative)) {
+					multiFacing.setFace(face, true);
+				}
+			}
+			b.setBlockData(multiFacing);
+		}
 		open = false;
+	}
+	
+	private boolean isSlab(Material material) {
+		// This is such jank but if it works...
+		return material.name().contains("SLAB");
 	}
 	
 	public boolean isOpen(){
